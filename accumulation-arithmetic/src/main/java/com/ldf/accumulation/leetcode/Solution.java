aa3d58e23fc831bf4f47b115dcc738330b1e669f
lidefu <lidefu@bonc.com.cn>
2018-11-15 09:08:14 +0800

    两数相加 官方解答

--- accumulation-arithmetic/src/main/java/com/ldf/accumulation/leetcode/Solution.java
+++ accumulation-arithmetic/src/main/java/com/ldf/accumulation/leetcode/Solution.java
@@ -29,7 +29,7 @@ public class Solution {
         Solution solution = new Solution();
         ListNode l1 = solution.listNode("342");
         ListNode l2 = solution.listNode("465");
-        ListNode listNode = solution.addTwoNumbers2(l1, l2);
+        ListNode listNode = solution.addTwoNumbersOfficial(l1, l2);
         System.out.println(1);
     }
 
@@ -48,6 +48,65 @@ public class Solution {
         long sum = i1 + i2;
         listNode = listNode(String.valueOf(sum));
         return listNode;
+    }
+
+    /**
+     * 思路2：模拟两数相加(成功 但是太繁琐了，多了很多不必要的循环)
+     * 得到l1、l2对应的数字 然后将其转为两个数组 新建一个数组存储他们的和的各个位数的值
+     * @param l1
+     * @param l2
+     * @return
+     */
+    private ListNode addTwoNumbers2(ListNode l1, ListNode l2){
+        ListNode listNode = null;
+        List<Integer> intL1 = getIntegerList(l1);
+        List<Integer> intL2 = getIntegerList(l2);
+        List<Integer> sumList = getIntegerListSum(intL1, intL2);
+        StringBuilder sb = new StringBuilder();
+        sumList.stream().forEach(sum -> {
+            sb.append(sum);
+        });
+        listNode = listNode(sb.toString());
+        return listNode;
+    }
+
+    /**
+     * 官方解答
+     * 伪代码如下：
+         将当前结点初始化为返回列表的哑结点。
+         将进位 carrycarry 初始化为 00。
+         将 pp 和 qq 分别初始化为列表 l1l1 和 l2l2 的头部。
+         遍历列表 l1l1 和 l2l2 直至到达它们的尾端。
+         将 xx 设为结点 pp 的值。如果 pp 已经到达 l1l1 的末尾，则将其值设置为 00。
+         将 yy 设为结点 qq 的值。如果 qq 已经到达 l2l2 的末尾，则将其值设置为 00。
+         设定 sum = x + y + carrysum=x+y+carry。
+         更新进位的值，carry = sum / 10carry=sum/10。
+         创建一个数值为 (sum \bmod 10)(summod10) 的新结点，并将其设置为当前结点的下一个结点，然后将当前结点前进到下一个结点。
+         同时，将 pp 和 qq 前进到下一个结点。
+         检查 carry = 1carry=1 是否成立，如果成立，则向返回列表追加一个含有数字 11 的新结点。
+         返回哑结点的下一个结点。
+         请注意，我们使用哑结点来简化代码。如果没有哑结点，则必须编写额外的条件语句来初始化表头的值。
+     * @param l1
+     * @param l2
+     * @return
+     */
+    private ListNode addTwoNumbersOfficial(ListNode l1, ListNode l2){
+        ListNode result = new ListNode(0);
+        ListNode p1 = l1,p2 = l2;
+        ListNode current = result; //当前所处节点
+        int extra = 0; //进位
+        while (p1 != null || p2 != null){
+            int p1Val = (p1 == null) ? 0 : p1.val;
+            int p2Val = (p2 == null) ? 0 : p2.val;
+            int currentVal = p1Val + p2Val + extra;
+            current.next = new ListNode(currentVal%10);
+            current = current.next; //切换当前节点(关键)
+            extra = currentVal/10;
+            if(p1 != null) p1 = p1.next;
+            if(p2 != null) p2 = p2.next;
+        }
+        if(extra == 1) current.next = new ListNode(1);
+        return result.next;
     }
 
     /**
@@ -91,26 +150,6 @@ public class Solution {
                 listNode = currentListNode;
             }
         }
-        return listNode;
-    }
-
-    /**
-     * 思路2：模拟两数相加
-     * 得到l1、l2对应的数字 然后将其转为两个数组 新建一个数组存储他们的和的各个位数的值
-     * @param l1
-     * @param l2
-     * @return
-     */
-    private ListNode addTwoNumbers2(ListNode l1, ListNode l2){
-        ListNode listNode = null;
-        List<Integer> intL1 = getIntegerList(l1);
-        List<Integer> intL2 = getIntegerList(l2);
-        List<Integer> sumList = getIntegerListSum(intL1, intL2);
-        StringBuilder sb = new StringBuilder();
-        sumList.stream().forEach(sum -> {
-            sb.append(sum);
-        });
-        listNode = listNode(sb.toString());
         return listNode;
     }
 
